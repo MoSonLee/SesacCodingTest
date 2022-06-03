dc8eb20711c64cc72130d6de7945089969eb7019
SH <ron222@naver.com>
2022-06-03 09:33:49 +0000

    edit readme + add type logic

---
+++ main.swift
@@ -0,0 +1,14 @@
+// 1. 회원가입 : 닉네임, 이메일, 나이, 평점, 성별
+
+var nickname: String = "MosonLee"
+var age: Int = 27
+let email: String = "ron222@naver.com"
+var rate: Double = 5.0
+var gender: Bool = true //ture:남자, false:여자
+
+print("안녕하세요 \(nickname)입니다.\n저는 \(age)살이고 이메일은 \(email)입니다. 평점은 \(rate)입니다.\n")
+
+nickname = "MosonLEEE"
+age = 28
+
+print("\(nickname) \(age) \(gender)")
