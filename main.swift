59730a66f2fa4039a4c9c0a2daf45bd9afa136e1
SH <ron222@naver.com>
2022-06-04 18:01:25 +0000

    edit readme

--- main.swift
+++ main.swift
@@ -8,3 +8,14 @@ var movieRate = Double.random(in: 1...5)
 
 print("영화 갯수는 \(movieCount), 영화 시간은 \(movieRuntime), 장르는 \(movieGenre), 평점은 \(movieRate)")
 
+//최근 검색어
+var searchList: [String] = ["원피스", "바지","상의"]
+searchList.insert("외투", at: 0)
+searchList.insert("양말", at: 2)
+searchList.insert(contentsOf:["바이","바위"] , at:0 )
+searchList.append("신발")
+searchList.append(contentsOf: ["하의", "하이", "하위"])
+// searchList.removeAll()
+searchList.remove(at: 0)
+
+print(searchList)
